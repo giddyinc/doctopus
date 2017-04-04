5f78e437f75a7c1b4d21e1b68ec41b6403d79364
Ben Lugavere <b.lugavere@gmail.com>
2017-04-04 17:13:59 -0400

    v0

--- test/index.js
+++ test/index.js
@@ -3,14 +3,38 @@
 
 const doctopus = require('../lib');
 const expect = require('expect');
+const Doc = doctopus.Doc;
+const DocBuilder = doctopus.DocBuilder;
+
+/**
+ * mocha --require babel-register test --watch
+ */
 
 describe('doctopus', function () {
   describe('export', () => {
+    let docs;
     beforeEach(() => {
       // doctopus
+      docs = new DocBuilder();
     });
+    it('should do', () => {
+      const docFactory = new Doc()
+        .group('Cats')
+        .setModel('Cat');
 
-    // it('should export something', () => expect(doctopus).toExist());
+      docs.add('/v1/cats', docFactory.get()
+        .description('Find All')
+        .summary('Find All')
+        .onSuccess(200, {
+          description: 'Response',
+          schema: Doc.arrayOf(Doc.model('Cat'))
+        })
+        .build());
+
+      const endResult = docs.build();
+      expect(endResult.info).toExist();
+      expect(endResult.paths).toExist();
+    });
   });
 
   describe('configuration', () => {
